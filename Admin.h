e45b8f502494b1d52588a420556f3f1ba0e2ebd2
Hai_Dev <nguyenhai25012000@gmail.com>
2024-04-08 13:24:27 +0700

    Update Interface User.h and Admin.h

--- Admin.h
+++ Admin.h
@@ -4,11 +4,27 @@
 #include "User.h"
 using namespace std;
 
-class Admin : public User{
+class Admin : virtual public User
+{
 private:
-    
+    Admin() : User("admin", "Admin123") {}
+    static Admin* instance;
+    vector<User> users;
+    vector<Travel> travels;
+    vector<Hotel> hotels;
+    vector<Transport> transports;
 public:
-
+    static Admin& getInstance();
+    void showListInfoUser();
+    void changeUser(User& user);
+    void deleteAccountUser(User& user);
+    void editTravelInfo(Travel& travel);
+    void editHotelInfo(Hotel& hotel);
+    void viewServiceInfo();
+    void addService(Travel& travel, Hotel& hotel, Transport& transport);
+    void deleteService(Travel& travel, Hotel& hotel, Transport& transport);
+    void loadData();
+    void saveData();
 };
 
 #endif // ADMIN_H
--- User.h
+++ User.h
@@ -6,20 +6,44 @@
 #include "Travel.h"
 using namespace std;
 
-class User{
+class User
+{
 private:
     string account;
     string password;
     string fullName;
     string address;
-    vector<Travel> myTravel;
     int age;
     int numMember;
+    vector<Travel> myTravel;
 public:
-    void getAccount();
+    /* Constructor */
+    User() {}
+    User(string& account, string& password) : account(account), password(password) {}
+    User(string& account, string& password, string& fullName, string& address, int age, int numMember, vector<Travel>& myTravel)
+    : account(account), password(password), fullName(fullName), address(address), age(age), numMember(numMember), myTravel(myTravel) {}
+    /* Destructor */
+    virtual ~User() {}
+    /* Change user infomation */
     void changeInfo();
+    /* Displays traveling information */
     void showTravelInfo();
-    void changeTravelInfo();
-    void cancleTravel();
+    /* Change traveling information */
+    void changeTravelInfo(const Travel& travel);
+    /* Cancel the travel */
+    void cancelTravel(const Travel& travel);
+    bool validateAccount() const;
+    /* Getter and setter */
+    const string& getAccount() const { return account; }
+    const string& getPassword() const { return password; }
+    void setPassword(const string& newPassword) { password = newPassword; }
+    const string& getFullname() const { return fullName; }
+    void setFullname(const string& newFullName) { fullName = newFullName; }
+    const string& getAddress() const { return address; }
+    void setAddress(const string& newAddress) { address = newAddress; }
+    int getAge() const { return age; }
+    void setAge(int newAge) { age = newAge; }
+    int getNumMember() const { return numMember; }
+    void setNumMember(int newNumMember) { numMember = newNumMember; }
 };
 #endif // USER_H
