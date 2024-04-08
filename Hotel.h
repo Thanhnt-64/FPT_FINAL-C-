db3b20f2eaf6a20e037859a8b1607d2762c7e2c5
ThanhTK <kthanh132@gmai.com>
2024-04-08 14:25:52 +0700

    Room and Hotel Class v1

--- Hotel.h
+++ Hotel.h
@@ -1,7 +1,17 @@
 #ifndef HOTEL_H
 #define HOTEL_H
+#include <iostream>
+#include <iomanip>
+using namespace std;
 
 class Hotel{
+    vector <Room> rooms;
+    string name;
+    string address;
+    int total_cost;
+    public:
+    void showRoomInfo();
+
 
 };
 #endif // HOTEL_H
---
+++ Room.cpp
@@ -0,0 +1,51 @@
+#include "Room.h"
+
+Room ::Room(string name = nonestring, string startTime = nonestring, string endTime = nonestring, string typeRoom = nonestring)
+    : m_name{name}, m_startTime{startTime}, m_endTime{endTime}, m_typeRoom{typeRoom}
+{
+}
+
+void Room::setName(string name)
+{
+    m_name = name;
+}
+
+void Room::setStartTime(string startTime)
+{
+    m_startTime = startTime;
+}
+
+void Room::setEndTime(string endTime)
+{
+    m_endTime = endTime;
+}
+
+void Room::setTypeRoom(string typeRoom)
+{
+    m_typeRoom = typeRoom;
+}
+
+void Room::setEndTime(string endTime)
+{
+    m_endTime = endTime;
+}
+
+string Room::getName()
+{
+    return m_name;
+}
+
+string Room::getStartTime()
+{
+    return  m_startTime;
+}
+
+string Room::getEndTime()
+{
+    return m_endTime;
+}
+
+string Room::getTypeRoom()
+{
+    return m_typeRoom;
+}
