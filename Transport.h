6692c066cef2878edff6950e2d1ff24bb72ae54e
quyen123 <thequyen598@gmail.com>
2024-04-09 14:38:57 +0700

    quyen

--- Transport.h
+++ Transport.h
@@ -26,6 +26,18 @@ public:
     void validate_destination(string &);
     void validate_cost();
     void validate(string&);
+	void change_name(string &);
+	void change_brand(string&);
+	void change_from(string&);
+	void change_destination(string &);
+	void change_time(string &);
+	void change_cost(int &);
+	string get_name();
+	string get_brand();
+	string get_destination();
+	string get_from();
+	string get_time();
+	int get_cost();
 	//Transport Name |Brand |From |Destination |Cost |Time
 	//20 digit      |20 digit |20 digit |20 digit |20 digit |20 digit
     void output();
