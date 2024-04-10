27b976726a8239e2d5bb8fc92691734287e60a4c
thanh <nguyenthanh3557@gmail.com>
2024-04-10 15:15:27 +0700

    fix read fiel

--- Travel.h
+++ Travel.h
@@ -11,6 +11,7 @@ class Travel{
         string namePlace;
         int travelCost;
     public:
+        Travel(){}
         Travel(vector <Hotel> hotel,vector <Transport> trans );
         //Show travel info
         // Hotel Name|Room Name |Type Room |Start Time  |End Time  |Cost |Transport |Brand |From |To |Time |Cost     
@@ -21,14 +22,13 @@ class Travel{
             cout << "Enter new place: ";
             getline(cin, namePlace);
         }
+        void setPlace(string place){
+            namePlace = place;
+        }
         string getPlace(){
             return namePlace;
         }
-<<<<<<< HEAD
-        void setPlace(const string& newPlace) { namePlace = newPlace; }
-=======
         int getTravelCost(){return (travelCost = hotels[0].getTotalCost() + transports[0].get_cost());}
->>>>>>> f3326b47c053372425382a9ae35ef4598b7d4236
 };
 typedef Travel Service;
 #endif // TRAVEL_H
--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
-#include "Admin.c++"
-#include "Hotel.c++"
-#include "Room.cpp"
-#include "System.c++"
-#include "Travel.cpp"
-#include "User.c++"
-#include "File.cpp"
+#include "Admin.h"
+#include "Hotel.h"
+#include "Room.h"
+#include "System.h"
+#include "Travel.h"
+#include "User.h"
+#include "File.h"
 bool checkAccountAdmin(const string &s){
     return s == "admin";
 }
@@ -16,8 +16,8 @@ int main()
     vector<Travel> travels;
 
     //Load data from files
-    Read_User read_user("user_database.json");
-    Read_Service read_service("service_database.json");
+    Read_User read_user("database_user.json");
+    Read_Service read_service("database_service.json");
     users=read_user.read_Users();
     travels=read_service.read_Travels();
     read_user.~Read_User();
@@ -28,35 +28,5 @@ int main()
     User u1;
     string account;
     int choose;
-    while(1){
-        cout << setw(5) << "Menu:\n";
-        cout << setw(10) << "1. Login\n";
-        cout << setw(10) << "2. Register\n";
-        cout << setw(10) << "Choose 1 or 2\n";
-        cin >> choose; cin.ignore();
-        switch (choose)
-        {
-        case 1:
-            cout << "Enter your account: "; getline(cin, account);
-            if(checkAccountAdmin(account)){
-                cout << "You are login as admin!\n";
-                sys.logInAccount();
-                sys.runAdmin(travels, users);
-                break;
-            }
-            user = sys.logInAccount(users, u1);
-            break;
-        case 2:
-            user = sys.registerAccount(users, u1);
-            cout << "Register successfully!\n";
-            cout << "Please log in with your account!\n";
-            user = sys.logInAccount(users, u1);
-            sys.run(travels, user);
-            break;
-        default:
-            break;
-        }
-        system("cls");
-    }
     return 0;
 }
